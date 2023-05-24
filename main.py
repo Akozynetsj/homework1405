dd00350d39b0a811d58325ce8ccdd188d6042634
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-24 15:58:39 +0300

    ex4

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-#3
-class Animals:
-    pass
-class Dog(Animals):
-    def sound(self):
-        print("Песик каже: Гав")
-bobik = Dog()
-bobik.sound()
+#4
+class BankAccount:
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        if self.balance - amount < 0:
+            print("Недостатньо коштів")
+        else:
+            self.balance -= amount
+    def check_balance(self):
+        return self.balance
+name = BankAccount("Anastasiya", 5000)
+name.deposit(235)
+name.withdraw(100)
+print(name.check_balance())
